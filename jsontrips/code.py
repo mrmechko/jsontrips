c640e7fcd156e18ba4f526578d45d08f372bf225
Rik <rbose@cs.rochester.edu>
2019-01-14 22:20:27 -0500

    open the actual file

--- jsontrips/code.py
+++ jsontrips/code.py
@@ -1,4 +1,5 @@
+import os
 import pkg_resources
 
 def get_file(name):
-    return open(pkg_resources.resource_string(__name__, name))
+    return open(pkg_resources.resource_string(__name__, os.path.join("data", name)))

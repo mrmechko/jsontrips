c04448537075f50b35de06049144da0f380b2289
Rik <rbose@cs.rochester.edu>
2019-01-14 22:32:53 -0500

    code resource as string not bytes

--- jsontrips/code.py
+++ jsontrips/code.py
@@ -3,4 +3,4 @@ import pkg_resources
 import json
 
 def get_file(name):
-    return json.loads(pkg_resources.resource_string(__name__, os.path.join("data", name)))
+    return json.loads(pkg_resources.resource_string(__name__, os.path.join("data", name)).decode('utf-8'))

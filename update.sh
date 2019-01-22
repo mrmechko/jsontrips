ef08b4d2e64f55bf501d4f08b83a3f1a893e61ba
Rik <rbose@cs.rochester.edu>
2019-01-22 16:22:15 -0500

    update script works

--- update.sh
+++ update.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -e # exit if anything breaks
+
 rm -rf lisp
 rm -rf dist
 
 mkdir lisp
 mkdir dist
 
-python code/generator/generator.py -f generator/template.lisp > lisp/genontology.lisp
-python code/generator/generator.py -f generator/lextemplate.lisp > lisp/genlexicon.lisp
+python code/generator/generator.py -f code/generator/template.lisp > lisp/genontology.lisp
+python code/generator/generator.py -f code/generator/lextemplate.lisp > lisp/genlexicon.lisp
 
 sbcl --script lisp/genontology.lisp
 sbcl --script lisp/genlexicon.lisp

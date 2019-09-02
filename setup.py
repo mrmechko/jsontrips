f7e73b384f575fccc899ee4f9880addda969c7c4
Rik <rbose@cs.rochester.edu>
2019-09-01 21:46:23 -0400

    'automatic update of jsontrips [2019-09-01 21:46:23]'

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 if __name__ == "__main__":
         setup(
                 name='jsontrips',
-                version="0.1.20",
+                version="0.1.21",
                 author="Rik",
                 author_email="rbose@cs.rochester.edu",
                 description="A basic package",
